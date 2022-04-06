3ea01ee2c2c1ce95b0e0492309901ed5754c0110
Vladyslav Chyzhevskyi <vladyslav@chyzhevskyi.com>
2022-04-06 14:07:49 +0200

    refactor components package

--- packages/components/src/Test/index.tsx
+++ packages/components/src/Test/index.tsx
@@ -1,10 +1,1 @@
-// import React from 'react'
-
-const Test = ({className}: { className?: string }) => {
-    return (
-       <>
-           <h1 className={className}>THIS IS TEST COMPONENT</h1>
-       </>
-    )
-}
-export default Test
+export * from './Test'
