19ae1ec28f6f4a4eb0434814afc22b1fe448ce6c
Vladyslav Chyzhevskyi <vladyslav@chyzhevskyi.com>
2022-06-15 00:00:43 +0200

    added storybook to project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard'],
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:storybook/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
---
+++ packages/components/src/components/Loader/index.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Meta } from '@storybook/react'
+
+import { Loader } from './index'
+
+export default {
+  title: 'Loader',
+  component: Loader,
+} as Meta
+
+export const Basic = () => <Loader />
