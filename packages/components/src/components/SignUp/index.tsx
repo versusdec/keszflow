ed470d0ed7ad1706766834a734f9276fb16192bd
versusdec <versusdec@gmail.com>
2022-06-17 11:10:46 +0300

    layout and drawer width

--- packages/components/src/components/SignUp/index.tsx
+++ packages/components/src/components/SignUp/index.tsx
@@ -1,13 +1,10 @@
-import { Box, Typography, Drawer, Button } from '@mui/material'
+import { Box, Typography, Button } from '@mui/material'
 import Link from 'next/link'
 import { Input } from '../../elements/input'
 import * as Yup from 'yup'
 import { Formik, Form, FormikHelpers, FormikProps } from 'formik'
-import { KeszflowBusinessLogo } from '../KeszflowBusinessLogo'
 
 export const SignUpJSX = () => {
-  const drawerWidth = 375
-
   const initialValues = {
     email: '',
     password: '',
@@ -26,99 +23,59 @@ export const SignUpJSX = () => {
 
   return (
     <>
-      <Box
-        sx={{
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          right: 0,
-          left: 0,
-          display: 'flex',
-          flexGrow: 1,
-          bgcolor: 'action.focus',
+      <Typography variant={'h3'} mb={3}>
+        SIGN UP
+      </Typography>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values: any, formikHelpers: FormikHelpers<any>) => {
+          console.log(values)
+          formikHelpers.setSubmitting(false)
         }}
       >
-        <Drawer
-          variant="permanent"
-          anchor="left"
-          sx={{
-            maxWidth: drawerWidth,
-            width: '100%',
-            '& .MuiDrawer-paper': {
-              maxWidth: drawerWidth,
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              p: 3,
-            },
-          }}
-        >
-          <Typography variant={'h3'} mb={3}>
-            SIGN UP
-          </Typography>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={(values: any, formikHelpers: FormikHelpers<any>) => {
-              console.log(values)
-              formikHelpers.setSubmitting(false)
-            }}
-          >
-            {(formikProps: FormikProps<any>) => (
-              <Form>
-                <Box sx={{}}>
-                  <Input
-                    label={'Email'}
-                    margin={'normal'}
-                    name={'email'}
-                    fullWidth
-                  />
-                  <Input
-                    label={'Password'}
-                    margin={'normal'}
-                    name={'password'}
-                    type={'password'}
-                    fullWidth
-                  />
-                  <Input
-                    label={'Confirm Password'}
-                    margin={'normal'}
-                    name={'confirmPassword'}
-                    type={'password'}
-                    fullWidth
-                  />
+        {(formikProps: FormikProps<any>) => (
+          <Form>
+            <Box sx={{}}>
+              <Input
+                label={'Email'}
+                margin={'normal'}
+                name={'email'}
+                fullWidth
+              />
+              <Input
+                label={'Password'}
+                margin={'normal'}
+                name={'password'}
+                type={'password'}
+                fullWidth
+              />
+              <Input
+                label={'Confirm Password'}
+                margin={'normal'}
+                name={'confirmPassword'}
+                type={'password'}
+                fullWidth
+              />
 
-                  <Box>
-                    <Typography variant={'caption'}>
-                      By pressing the button you confirm our{' '}
-                      <Link href={'#'}>Terms and Conditions</Link>,{' '}
-                      <Link href={'#'}>Privacy Policy</Link> and{' '}
-                      <Link href={'#'}>Rules</Link> of our service.
-                    </Typography>
-                  </Box>
-                  <Button variant={'contained'} type={'submit'} sx={{ mt: 3 }}>
-                    SIGN UP
-                  </Button>
-                  <Box sx={{ mt: 10 }}>
-                    Already have account? <Link href={'/sign-in'}>Sign in</Link>
-                  </Box>
-                </Box>
-              </Form>
-            )}
-          </Formik>
-        </Drawer>
-        <Box
-          sx={{
-            width: '66%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <KeszflowBusinessLogo width={365} />
-        </Box>
-      </Box>
+              <Box>
+                <Typography variant={'caption'}>
+                  By pressing the button you confirm our{' '}
+                  <Link href={'#'}>Terms and Conditions</Link>,{' '}
+                  <Link href={'#'}>Privacy Policy</Link> and{' '}
+                  <Link href={'#'}>Rules</Link> of our service.
+                </Typography>
+              </Box>
+              <Button variant={'contained'} type={'submit'} sx={{ mt: 3 }}>
+                SIGN UP
+              </Button>
+              <Box sx={{ mt: 10 }}>
+                Already have account? <Link href={'/sign-in'}>Sign in</Link>
+              </Box>
+            </Box>
+          </Form>
+        )}
+      </Formik>
     </>
   )
 }
--- packages/components/src/layouts/View/index.tsx
+++ packages/components/src/layouts/View/index.tsx
@@ -1,8 +1,10 @@
 import { ReactElement } from 'react'
 import { Footer, Header } from '@keszflow/components'
 import Head from 'next/head'
-import { Box } from '@mui/material'
+import { Box, Drawer } from '@mui/material'
 import { InvoiceUpload, useUploadModal } from '../../components/Invoices/Upload'
+import { KeszflowBusinessLogo } from '../../components/KeszflowBusinessLogo'
+import { useRouter } from 'next/router'
 
 interface Props {
   children?: ReactElement
@@ -10,9 +12,21 @@ interface Props {
 }
 
 export default function Layout(props: Props) {
+  const router = useRouter()
   const { uploadModalOpen, handleUploadModal } = useUploadModal()
+  const auth = router.pathname === '/sign-in' || router.pathname === '/sign-up'
+  const head = (
+    <Head>
+      <title>{props.title ? props.title + ' | Keszflow' : 'Keszflow'} </title>
+      <link rel="icon" href="/img/keszflow_logo.png" type="image/png" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+    </Head>
+  )
 
-  return (
+  const defaultLayout = (
     <>
       <Box
         onDragEnter={(e) => {
@@ -21,16 +35,7 @@ export default function Layout(props: Props) {
           handleUploadModal()
         }}
       >
-        <Head>
-          <title>
-            {props.title ? props.title + ' | Keszflow' : 'Keszflow'}{' '}
-          </title>
-          <link rel="icon" href="/img/keszflow_logo.png" type="image/png" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-        </Head>
+        {head}
         <Box
           sx={{
             bgcolor: 'background.paper',
@@ -53,4 +58,55 @@ export default function Layout(props: Props) {
       <InvoiceUpload open={uploadModalOpen} onClose={handleUploadModal} />
     </>
   )
+
+  const drawerWidth = 500
+
+  const authLayout = (
+    <>
+      {head}
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          display: 'flex',
+          flexGrow: 1,
+          bgcolor: 'action.focus',
+        }}
+      >
+        <Drawer
+          variant="permanent"
+          anchor="left"
+          sx={{
+            maxWidth: drawerWidth,
+            width: '100%',
+            '& .MuiDrawer-paper': {
+              maxWidth: drawerWidth,
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              p: 3,
+            },
+          }}
+        >
+          {props.children}
+        </Drawer>
+        <Box
+          sx={{
+            width: '66%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <KeszflowBusinessLogo width={365} />
+        </Box>
+      </Box>
+    </>
+  )
+
+  return <>{auth ? authLayout : defaultLayout}</>
 }
