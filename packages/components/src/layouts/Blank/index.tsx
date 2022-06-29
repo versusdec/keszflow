4ced60ac932e044e6d4b44fc5fa7e87d18e9cb49
versusdec <versusdec@gmail.com>
2022-06-29 16:19:43 +0300

    removed comments

--- packages/components/src/layouts/Blank/index.tsx
+++ packages/components/src/layouts/Blank/index.tsx
@@ -18,22 +18,7 @@ export default function BlankLayout(props: Props) {
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
       </Head>
-      <Box
-        sx={
-          {
-            /* position: 'absolute',
-          top: 0,
-          bottom: 0,
-          right: 0,
-          left: 0,
-          display: 'flex',
-          flexGrow: 1,
-          bgcolor: 'action.focus', */
-          }
-        }
-      >
-        {props.children}
-      </Box>
+      <Box>{props.children}</Box>
     </>
   )
 }
